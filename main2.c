92a3f8f848b8617ba61cfe4763b7485cfc3608d7
Eduardo Martins <eduardo.hmartins93@gmail.com>
2017-03-31 13:14:18 -0300

    main2

---
+++ main2.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main (void) {
+
+
+	double nota1;
+	double nota2;
+	scanf("%lf %lf", &nota1, &nota2);
+	double MEDIA = nota1*3.5 + nota2*7.5;
+	printf("MEDIA = %.5f/n", MEDIA /11);
+
+}
+	
